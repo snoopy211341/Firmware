02999d51c9c354b9bdc3173c7827cbf13c04b410
Daniel Agar <daniel@agar.ca>
2020-02-27 11:56:39 +0100

    WIP: new bmi088

--- boards/px4/fmu-v5x/default.cmake
+++ boards/px4/fmu-v5x/default.cmake
@@ -31,10 +31,11 @@ px4_add_board(
 		imu/adis16448
 		imu/adis16477
 		imu/adis16497
-		imu/bmi088
+		#imu/bmi088
+		imu/bosch/bmi088
 		#imu/mpu6000 # legacy icm20602 driver
-		imu/invensense/icm20602
-		imu/st/ism330dlc
+		#imu/invensense/icm20602
+		#imu/st/ism330dlc
 		irlock
 		lights/blinkm
 		lights/rgbled
